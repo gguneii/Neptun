7a1bfba1f8d653fcb032ecd9a3e731fbba1b90d5
sabiinajm <sabiinajm@gmail.com>
2024-10-28 18:07:03 +0400

    fetching from api

--- src/components/main/BestSellers.jsx
+++ src/components/main/BestSellers.jsx
@@ -1,21 +1,27 @@
 import { FaSearch } from "react-icons/fa";
 import { Autoplay, Navigation, Pagination, Scrollbar } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/effect-fade';
+import { useEffect, useState } from "react";
 
 function BestSellers() {
+  const [PRODUCT, setPRODUCT] = useState([]);
+
+  useEffect(() => {
+    fetch('https://neptunbk.vercel.app/products?limit=100')
+      .then(res => res.json())
+      .then(data => setPRODUCT(data.products))
+  }, []);
+
   return (
     <div className="bg-gray-100 h-full">
       <div className="max-w-[1200px] w-[95%] mx-auto py-4">
         <div className="flex items-center justify-between py-4">
           <h3 className="font-semibold">Ən çox satılanlar</h3>
-          <div>
-            <p className="text-[#ff8230] cursor-pointer">Hamsına bax</p>
-          </div>
+          <p className="text-[#ff8230] cursor-pointer">Hamsına bax</p>
         </div>
         <Swiper
           className=" h-[400px] mb-6 lgx:mr-2 lgx:mb-0 w-full"
@@ -25,22 +31,14 @@ function BestSellers() {
           }}
           modules={[Navigation, Pagination, Scrollbar, Autoplay]}
           breakpoints={{
-            400:{
-              slidesPerView: 2,
-            },
-            640: {
-              slidesPerView: 3,
-            },
-            1024: {
-              slidesPerView: 4,
-            }
+            400: { slidesPerView: 2 },
+            640: { slidesPerView: 3 },
+            880: { slidesPerView: 4 },
+            1180: { slidesPerView: 5 },
+            1230: { slidesPerView: 6 },
           }}
           spaceBetween={18}
-          autoplay={{
-            delay: 3000,
-            disableOnInteraction: false,
-          }}
-          loop={true}
+          autoplay={{ delay: 3000, disableOnInteraction: false }}
           speed={1000}
           navigation={{
             nextEl: '.swiper-button-next.swiper-button-2',
@@ -48,222 +46,51 @@ function BestSellers() {
           }}
         >
           <div className="flex">
-            <SwiperSlide className="relative">
-              <div className="bg-white border-[1px] rounded-md flex flex-col items-center justify-center w-full md:w-[200px] lg:w-[250px]">
-                <div className="flex w-[80%] mt-4 justify-end">
-                  <div className="w-[21.6px] h-[22px]">
-                    <svg
-                      className="fill-transparent object-cover stroke-[#ff8230] stroke-[8.07px] hover:fill-[#ff8230] duration-200"
-                      xmlns="http://www.w3.org/2000svg"
-                      viewBox="-5 0 156.69 110.07">
-                      <defs></defs>
-                      <path
-                        data-name="neptun_heart"
-                        className="cls-1"
-                        d="M1322.95,268.738c-7.63,17.621-62.02,55.614-62.94,56.251V325a0.011,0.011,0,0,0-.02,0v-0.015c-0.92-.637-55.31-38.63-62.94-56.251a34.807,34.807,0,0,1,18.68-45.924A35.749,35.749,0,0,1,1260,236.828a35.749,35.749,0,0,1,44.27-14.014A34.807,34.807,0,0,1,1322.95,268.738Z"
-                        transform="translate(-1191.655 -217.465)"></path>
-                    </svg>
-                  </div>
-                </div>
-                <div className=" relative w-[100px] h-[140px] ">
-                  <img className="object-cover" src="../src/assets/abc.webp" alt="" />
-                  <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 translate-y-[-50px] hover:translate-y-[-10px] transition-all duration-500">
-                    <div className="bg-[#ff8230] w-[35px] h-[35px] rounded-full flex justify-center items-center">
-                      <FaSearch className="text-white text-[.9rem]" />
+            {PRODUCT && PRODUCT.map((item) => (
+              <SwiperSlide key={item.id} className="relative">
+                       <div className="bg-white border-[1px] h-[350px] rounded-md flex flex-col items-center justify-center  w-full lgx:w-[190px]">
+                  <div className="flex w-[80%] mt-4 justify-end">
+                    <div className="w-[21.6px] h-[22px]">
+                      <svg
+                        className="fill-transparent object-cover stroke-[#ff8230] stroke-[8.07px] hover:fill-[#ff8230] duration-200"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="-5 0 156.69 110.07"
+                      >
+                        <path
+                          data-name="neptun_heart"
+                          className="cls-1"
+                          d="M1322.95,268.738c-7.63,17.621-62.02,55.614-62.94,56.251V325a0.011,0.011,0,0,0-.02,0v-0.015c-0.92-.637-55.31-38.63-62.94-56.251a34.807,34.807,0,0,1,18.68-45.924A35.749,35.749,0,0,1,1260,236.828a35.749,35.749,0,0,1,44.27-14.014A34.807,34.807,0,0,1,1322.95,268.738Z"
+                          transform="translate(-1191.655 -217.465)"
+                        ></path>
+                      </svg>
                     </div>
                   </div>
-                </div>
-                <h3 className="text-[0.65rem] font-semibold mb-4 px-4 text-center">ABC 750GR BULASIQ DETERJANI LIMON FERAHLIGI</h3>
-                <h2 className="text-[1.3rem] font-bold">2.90</h2>
-                <div className="flex justify-between items-center w-[110px]">
-                  <button className="text-[#ff8230] text-[2.3rem] font-bold">-</button>
-                  <span>1</span>
-                  <button className="text-[#ff8230] text-[2.2rem]  font-bold">+</button>
-                </div>
-                <button className="bg-[#ff8230] hover:bg-[#e4742a] transition duration-200 text-white rounded-full w-[100px] h-[35px] mb-10">Sebete al</button>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="bg-white border-[1px] rounded-md flex flex-col items-center justify-center w-full md:w-[200px] lg:w-[250px]">
-                <div className="flex w-[80%] mt-4 justify-end">
-                  <div className="w-[21.6px] h-[22px]">
-                    <svg
-                      className="fill-transparent object-cover stroke-[#ff8230] stroke-[8.07px] hover:fill-[#ff8230] duration-200"
-                      xmlns="http://www.w3.org/2000svg"
-                      viewBox="-5 0 156.69 110.07">
-                      <defs></defs>
-                      <path
-                        data-name="neptun_heart"
-                        className="cls-1"
-                        d="M1322.95,268.738c-7.63,17.621-62.02,55.614-62.94,56.251V325a0.011,0.011,0,0,0-.02,0v-0.015c-0.92-.637-55.31-38.63-62.94-56.251a34.807,34.807,0,0,1,18.68-45.924A35.749,35.749,0,0,1,1260,236.828a35.749,35.749,0,0,1,44.27-14.014A34.807,34.807,0,0,1,1322.95,268.738Z"
-                        transform="translate(-1191.655 -217.465)"></path>
-                    </svg>
-                  </div>
-                </div>
-                <div className=" relative w-[100px] h-[140px] ">
-                  <img className="object-cover" src="../src/assets/abc.webp" alt="" />
-                  <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 translate-y-[-50px] hover:translate-y-[-10px] transition-all duration-500">
-                    <div className="bg-[#ff8230] w-[35px] h-[35px] rounded-full flex justify-center items-center">
-                      <FaSearch className="text-white text-[.9rem]" />
+                  <div className="relative w-[100px] h-[140px]">
+                    <img className="object-cover" src={item.img} alt={item.name} />
+                    <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 translate-y-[-50px] hover:translate-y-[-10px] transition-all duration-500">
+                      <div className="bg-[#ff8230] w-[35px] h-[35px] rounded-full flex justify-center items-center">
+                        <FaSearch className="text-white text-[.9rem]" />
+                      </div>
                     </div>
                   </div>
-                </div>
-                <h3 className="text-[0.65rem] font-semibold mb-4 px-4 text-center">ABC 750GR BULASIQ DETERJANI LIMON FERAHLIGI</h3>
-                <h2 className="text-[1.3rem] font-bold">2.90</h2>
-                <div className="flex justify-between items-center w-[110px]">
-                  <button className="text-[#ff8230] text-[2.3rem] font-bold">-</button>
-                  <span>1</span>
-                  <button className="text-[#ff8230] text-[2.2rem]  font-bold">+</button>
-                </div>
-                <button className="bg-[#ff8230] hover:bg-[#e4742a] transition duration-200 text-white rounded-full w-[100px] h-[35px] mb-10">Sebete al</button>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="bg-white border-[1px] rounded-md flex flex-col items-center justify-center w-full md:w-[200px] lg:w-[250px]">
-                <div className="flex w-[80%] mt-4 justify-end">
-                  <div className="w-[21.6px] h-[22px]">
-                    <svg
-                      className="fill-transparent object-cover stroke-[#ff8230] stroke-[8.07px] hover:fill-[#ff8230] duration-200"
-                      xmlns="http://www.w3.org/2000svg"
-                      viewBox="-5 0 156.69 110.07">
-                      <defs></defs>
-                      <path
-                        data-name="neptun_heart"
-                        className="cls-1"
-                        d="M1322.95,268.738c-7.63,17.621-62.02,55.614-62.94,56.251V325a0.011,0.011,0,0,0-.02,0v-0.015c-0.92-.637-55.31-38.63-62.94-56.251a34.807,34.807,0,0,1,18.68-45.924A35.749,35.749,0,0,1,1260,236.828a35.749,35.749,0,0,1,44.27-14.014A34.807,34.807,0,0,1,1322.95,268.738Z"
-                        transform="translate(-1191.655 -217.465)"></path>
-                    </svg>
+                  <h3 className="text-[0.65rem] font-semibold mb-4 px-4 text-center">{item.name}</h3>
+                  <h2 className="text-[1.3rem] font-bold">{item.price}</h2>
+                  <div className="flex justify-between items-center w-[110px]">
+                    <button className="text-[#ff8230] text-[2.3rem] font-bold">-</button>
+                    <span>1</span>
+                    <button className="text-[#ff8230] text-[2.2rem] font-bold">+</button>
                   </div>
+                  <button className="bg-[#ff8230] hover:bg-[#e4742a] transition duration-200 text-white rounded-full w-[100px] h-[35px] mb-10">Sebete al</button>
                 </div>
-                <div className=" relative w-[100px] h-[140px] ">
-                  <img className="object-cover" src="../src/assets/abc.webp" alt="" />
-                  <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 translate-y-[-50px] hover:translate-y-[-10px] transition-all duration-500">
-                    <div className="bg-[#ff8230] w-[35px] h-[35px] rounded-full flex justify-center items-center">
-                      <FaSearch className="text-white text-[.9rem]" />
-                    </div>
-                  </div>
-                </div>
-                <h3 className="text-[0.65rem] font-semibold mb-4 px-4 text-center">ABC 750GR BULASIQ DETERJANI LIMON FERAHLIGI</h3>
-                <h2 className="text-[1.3rem] font-bold">2.90</h2>
-                <div className="flex justify-between items-center w-[110px]">
-                  <button className="text-[#ff8230] text-[2.3rem] font-bold">-</button>
-                  <span>1</span>
-                  <button className="text-[#ff8230] text-[2.2rem]  font-bold">+</button>
-                </div>
-                <button className="bg-[#ff8230] hover:bg-[#e4742a] transition duration-200 text-white rounded-full w-[100px] h-[35px] mb-10">Sebete al</button>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="bg-white border-[1px] rounded-md flex flex-col items-center justify-center w-full md:w-[200px] lg:w-[250px]">
-                <div className="flex w-[80%] mt-4 justify-end">
-                  <div className="w-[21.6px] h-[22px]">
-                    <svg
-                      className="fill-transparent object-cover stroke-[#ff8230] stroke-[8.07px] hover:fill-[#ff8230] duration-200"
-                      xmlns="http://www.w3.org/2000svg"
-                      viewBox="-5 0 156.69 110.07">
-                      <defs></defs>
-                      <path
-                        data-name="neptun_heart"
-                        className="cls-1"
-                        d="M1322.95,268.738c-7.63,17.621-62.02,55.614-62.94,56.251V325a0.011,0.011,0,0,0-.02,0v-0.015c-0.92-.637-55.31-38.63-62.94-56.251a34.807,34.807,0,0,1,18.68-45.924A35.749,35.749,0,0,1,1260,236.828a35.749,35.749,0,0,1,44.27-14.014A34.807,34.807,0,0,1,1322.95,268.738Z"
-                        transform="translate(-1191.655 -217.465)"></path>
-                    </svg>
-                  </div>
-                </div>
-                <div className=" relative w-[100px] h-[140px] ">
-                  <img className="object-cover" src="../src/assets/abc.webp" alt="" />
-                  <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 translate-y-[-50px] hover:translate-y-[-10px] transition-all duration-500">
-                    <div className="bg-[#ff8230] w-[35px] h-[35px] rounded-full flex justify-center items-center">
-                      <FaSearch className="text-white text-[.9rem]" />
-                    </div>
-                  </div>
-                </div>
-                <h3 className="text-[0.65rem] font-semibold mb-4 px-4 text-center">ABC 750GR BULASIQ DETERJANI LIMON FERAHLIGI</h3>
-                <h2 className="text-[1.3rem] font-bold">2.90</h2>
-                <div className="flex justify-between items-center w-[110px]">
-                  <button className="text-[#ff8230] text-[2.3rem] font-bold">-</button>
-                  <span>1</span>
-                  <button className="text-[#ff8230] text-[2.2rem]  font-bold">+</button>
-                </div>
-                <button className="bg-[#ff8230] hover:bg-[#e4742a] transition duration-200 text-white rounded-full w-[100px] h-[35px] mb-10">Sebete al</button>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="bg-white border-[1px] rounded-md flex flex-col items-center justify-center w-full md:w-[200px] lg:w-[250px]">
-                <div className="flex w-[80%] mt-4 justify-end">
-                  <div className="w-[21.6px] h-[22px]">
-                    <svg
-                      className="fill-transparent object-cover stroke-[#ff8230] stroke-[8.07px] hover:fill-[#ff8230] duration-200"
-                      xmlns="http://www.w3.org/2000svg"
-                      viewBox="-5 0 156.69 110.07">
-                      <defs></defs>
-                      <path
-                        data-name="neptun_heart"
-                        className="cls-1"
-                        d="M1322.95,268.738c-7.63,17.621-62.02,55.614-62.94,56.251V325a0.011,0.011,0,0,0-.02,0v-0.015c-0.92-.637-55.31-38.63-62.94-56.251a34.807,34.807,0,0,1,18.68-45.924A35.749,35.749,0,0,1,1260,236.828a35.749,35.749,0,0,1,44.27-14.014A34.807,34.807,0,0,1,1322.95,268.738Z"
-                        transform="translate(-1191.655 -217.465)"></path>
-                    </svg>
-                  </div>
-                </div>
-                <div className=" relative w-[100px] h-[140px] ">
-                  <img className="object-cover" src="../src/assets/abc.webp" alt="" />
-                  <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 translate-y-[-50px] hover:translate-y-[-10px] transition-all duration-500">
-                    <div className="bg-[#ff8230] w-[35px] h-[35px] rounded-full flex justify-center items-center">
-                      <FaSearch className="text-white text-[.9rem]" />
-                    </div>
-                  </div>
-                </div>
-                <h3 className="text-[0.65rem] font-semibold mb-4 px-4 text-center">ABC 750GR BULASIQ DETERJANI LIMON FERAHLIGI</h3>
-                <h2 className="text-[1.3rem] font-bold">2.90</h2>
-                <div className="flex justify-between items-center w-[110px]">
-                  <button className="text-[#ff8230] text-[2.3rem] font-bold">-</button>
-                  <span>1</span>
-                  <button className="text-[#ff8230] text-[2.2rem]  font-bold">+</button>
-                </div>
-                <button className="bg-[#ff8230] hover:bg-[#e4742a] transition duration-200 text-white rounded-full w-[100px] h-[35px] mb-10">Sebete al</button>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="bg-white border-[1px] rounded-md flex flex-col items-center justify-center w-full md:w-[200px] lg:w-[250px]">
-                <div className="flex w-[80%] mt-4 justify-end">
-                  <div className="w-[21.6px] h-[22px]">
-                    <svg
-                      className="fill-transparent object-cover stroke-[#ff8230] stroke-[8.07px] hover:fill-[#ff8230] duration-200"
-                      xmlns="http://www.w3.org/2000svg"
-                      viewBox="-5 0 156.69 110.07">
-                      <defs></defs>
-                      <path
-                        data-name="neptun_heart"
-                        className="cls-1"
-                        d="M1322.95,268.738c-7.63,17.621-62.02,55.614-62.94,56.251V325a0.011,0.011,0,0,0-.02,0v-0.015c-0.92-.637-55.31-38.63-62.94-56.251a34.807,34.807,0,0,1,18.68-45.924A35.749,35.749,0,0,1,1260,236.828a35.749,35.749,0,0,1,44.27-14.014A34.807,34.807,0,0,1,1322.95,268.738Z"
-                        transform="translate(-1191.655 -217.465)"></path>
-                    </svg>
-                  </div>
-                </div>
-                <div className=" relative w-[100px] h-[140px] ">
-                  <img className="object-cover" src="../src/assets/abc.webp" alt="" />
-                  <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 translate-y-[-50px] hover:translate-y-[-10px] transition-all duration-500">
-                    <div className="bg-[#ff8230] w-[35px] h-[35px] rounded-full flex justify-center items-center">
-                      <FaSearch className="text-white text-[.9rem]" />
-                    </div>
-                  </div>
-                </div>
-                <h3 className="text-[0.65rem] font-semibold mb-4 px-4 text-center">ABC 750GR BULASIQ DETERJANI LIMON FERAHLIGI</h3>
-                <h2 className="text-[1.3rem] font-bold">2.90</h2>
-                <div className="flex justify-between items-center w-[110px]">
-                  <button className="text-[#ff8230] text-[2.3rem] font-bold">-</button>
-                  <span>1</span>
-                  <button className="text-[#ff8230] text-[2.2rem]  font-bold">+</button>
-                </div>
-                <button className="bg-[#ff8230] hover:bg-[#e4742a] transition duration-200 text-white rounded-full w-[100px] h-[35px] mb-10">Sebete al</button>
-              </div>
-            </SwiperSlide>
-          </div >
+              </SwiperSlide>
+            ))}
+          </div>
           <div className="swiper-button-prev swiper-button-2" />
           <div className="swiper-button-next swiper-button-2" />
         </Swiper>
       </div>
     </div>
-  )
+  );
 }
-export default BestSellers
+
+export default BestSellers;
